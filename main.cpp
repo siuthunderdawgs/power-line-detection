ca5f80c4293cad8f5ec155ab0bd1d5bbf925c037
red-135 <red-135@hotmail.com>
2016-02-04 22:03:14 -0600

    Provides a slider that allows user to adjust threshold for probabilistic Hough line transform operating on a user-supplied image.

--- main.cpp
+++ main.cpp
@@ -9,32 +9,25 @@
 #include "LinePainter.h"
 #include "LineOverlayPainter.h"
 
-int main(int argc, char** argv)
+
+const int slider_max = 200;
+int slider;
+
+cv::Size image_size;
+cv::Mat image_src;
+cv::Mat image_mask;
+cv::Mat image_des;
+cv::Mat image_can;
+
+void on_trackbar(int pos, void *)
 {
-	const char* filename;
+	int thresh = pos + 10;
 
-	if(argc >= 2)
-		filename = argv[1];
-	else
-		filename = "image.jpg";
+	image_mask = cv::Scalar(0,0,0);
+	image_des = cv::Scalar(0,0,0);
 
-	cv::Mat image_src = cv::imread(filename);
-
-	if(image_src.empty())
-	{
-	 std::cout << "ERROR: Cannot open " << filename << std::endl;
-	 return -1;
-	}
-
-	cv::Size image_size = image_src.size();
-
-	cv::Mat image_mask = cv::Mat::zeros(image_size, CV_8UC3);
-	cv::Mat image_des = cv::Mat::zeros(image_size, CV_8UC3);
-	cv::Mat image_can = cv::Mat::zeros(image_size, CV_8UC3);
-
-	std::vector<cv::Vec2f> lines;
-	cv::Canny(image_src, image_can, 50, 200, 3);
-	cv::HoughLines(image_can, lines, 1, CV_PI/180, 100, 0, 0);
+	std::vector<cv::Vec4i> lines;
+	cv::HoughLinesP(image_can, lines, 1, CV_PI/180, thresh, 60, 1000);
 
 	LinePainter painter;
 	painter.SetImage(&image_mask);
@@ -47,12 +40,42 @@ int main(int argc, char** argv)
 	opainter.SetImageDes(&image_des);
 	opainter.DrawOverlay();
 
-
-	cv::namedWindow( "Display Image", CV_WINDOW_AUTOSIZE );
 	cv::imshow( "Display Image", image_des );
+}
 
-	cv::waitKey(0);
+
+int main(int argc, char** argv)
+{
+	const char* filename;
+
+	if(argc >= 2)
+		filename = argv[1];
+	else
+		filename = "image.jpg";
+
+	image_src = cv::imread(filename);
+
+	if(image_src.empty())
+	{
+	 std::cout << "ERROR: Cannot open " << filename << std::endl;
+	 return -1;
+	}
+
+	image_size = image_src.size();
+
+	image_mask = cv::Mat::zeros(image_size, CV_8UC3);
+	image_des = cv::Mat::zeros(image_size, CV_8UC3);
+
+	image_can = cv::Mat::zeros(image_size, CV_8UC3);
+	cv::Canny(image_src, image_can, 50, 200, 3);
+
+	cv::namedWindow( "Display Image");
+	cv::imshow( "Display Image", image_can );
+
+	slider = 0;
+
+	cv::createTrackbar("Hough Threshold = Position + 10", "Display Image", &slider, slider_max, on_trackbar);
+	while(char(cv::waitKey(1)) != 'q'){}
 
 	return 0;
 }
-
