d61ecbe5f588a8b2d1e506716b2f629573b430c5
red-135 <red-135@hotmail.com>
2016-02-23 11:50:09 -0600

    Write code that moves from window to window, computing the clutter measure and allowing the user to adjust the threshold. Allows the user to collect data that relates the clutter measure to the threshold.

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <assert.h>
 
 #include "LineSegment.h"
 #include "LinePainter.h"
 #include "LineOverlayPainter.h"
 #include "WindowedHoughTransform.h"
-
+#include "Windows.h"
 
 const int slider_max = 200;
 int slider;
@@ -20,32 +21,33 @@ cv::Mat image_mask;
 cv::Mat image_des;
 cv::Mat image_can;
 
+cv::Mat window;
+cv::Mat window_src;
+cv::Mat window_mask;
+cv::Mat window_des;
+
 void on_trackbar(int pos, void *)
 {
 	int thresh = pos + 20;
 
-	image_mask = cv::Scalar(0,0,0);
-	image_des = cv::Scalar(0,0,0);
+	window_mask = cv::Scalar(0,0,0);
+	window_des = cv::Scalar(0,0,0);
 
-	/*
 	std::vector<cv::Vec2f> lines;
-	cv::HoughLines(image_can, lines, 1, CV_PI/180, thresh, 0, 0);
+	cv::HoughLines(window, lines, 1, CV_PI/180, thresh, 0, 0);
 
 	LinePainter painter;
-	painter.SetImage(&image_mask);
+	painter.SetImage(&window_mask);
 	painter.SetLines(lines);
 	painter.DrawLines();
-	*/
-
-	WindowedHoughLine(image_can, image_mask, 8, 8, 1, CV_PI/180, thresh);
 
 	LineOverlayPainter opainter;
-	opainter.SetImageSrc(&image_src);
-	opainter.SetMask(&image_mask);
-	opainter.SetImageDes(&image_des);
+	opainter.SetImageSrc(&window_src);
+	opainter.SetMask(&window_mask);
+	opainter.SetImageDes(&window_des);
 	opainter.DrawOverlay();
 
-	cv::imshow( "Display Image", image_des );
+	cv::imshow( "Display Image", window_des );
 }
 
 
@@ -78,8 +80,34 @@ int main(int argc, char** argv)
 	cv::imshow( "Display Image", image_can );
 
 	slider = 0;
+	cv::createTrackbar(" ", "Display Image", &slider, slider_max, on_trackbar);
 
-	cv::createTrackbar("Hough Threshold = Position + 20", "Display Image", &slider, slider_max, on_trackbar);
+	std::vector< std::vector<cv::Mat> > windows, windows_src, windows_mask;
+	windows = CreateWindows(image_can, 4, 4);
+	windows_src = CreateWindows(image_src, 4, 4);
+	windows_mask = CreateWindows(image_mask, 4, 4);
+
+	for(unsigned int i = 0; i < windows.size(); i++)
+	{
+		for(unsigned int j = 0; j < windows[i].size(); j++)
+		{
+			slider = 0;
+
+			window = windows[i][j].clone();
+			window_src = windows_src[i][j].clone();
+			window_des = cv::Mat::zeros(window.size(), CV_8UC3);
+			window_mask = cv::Mat::zeros(window.size(), CV_8UC3);
+
+			double clutter_win = ComputeWindowClutter(window, 4,4);
+			std::cout << "Window [" << i << "][" << j << "]: ";
+			std::cout << "Clutter: " << clutter_win << "\n";
+
+			imshow("Display Image", window);
+			while(char(cv::waitKey(1)) != ' '){}
+		}
+	}
+
+	std::cout << "Done!";
 	while(char(cv::waitKey(1)) != 'q'){}
 
 	return 0;
